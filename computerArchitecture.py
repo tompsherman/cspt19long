fcdd5bbd654c85334f7f4f2afde84258308451c4
unknown <tompsherman@gmail.com>
2021-03-23 21:26:43 -0400

    store data

--- computerArchitecture.py
+++ computerArchitecture.py
@@ -26,24 +26,35 @@ and the decode cycle to take something from memory
 -- program counter to keep track of where we are in the memory at a given time
 -- state whether the system is running or not
 
+upcodes,
+if its in program how would you add it
+fetch decode execute
+
 """
 
 #some instructions for the cpu
 PRINT_TOM = 0b01011010 # print "tom" to console
 PRINT_NUM = 0b10010000 # print a number stored in ram 1 byte ahead of instruction
 HALT    =   0b10110111 # halt the operation of our data driven machine
-SAVE    =   0b00010001 # store a number from the ram 2 bytes ahead of instruction into a register denoted from 1 byte ahead of the instructionin ram
+STORE    =  0b00010001 # store a number from the ram 2 bytes ahead of instruction into a register denoted from 1 byte ahead of the instructionin ram
+PRINT_REG = 0b01111101 # print a number at the index into registers provided by the number from current instruction +1 in ram 
 
 #list for RAM
 ram = [
     PRINT_TOM,
     PRINT_NUM,
     23,
+    STORE,
+    1,
+    123,
     HALT
 ]
 
 # program counter (instruction pointer) to keep track of what we are executing
 pc = 0
+
+# registers
+registers = [0] * 8 # reg0 - reg7
 
 #loop for CPU execute cycle
 while True:
@@ -65,6 +76,13 @@ while True:
         num = ram[pc+1]
         print(num)
         pc += 2   
+    # DECODE
+    elif instruction == STORE:
+        # EXECUTE
+        reg_index = ram[pc+1]
+        num = ram[pc+2]
+        registers[reg_index] = num
+        pc += 3     
     # DECODE
     else:
         # EXECUTE
